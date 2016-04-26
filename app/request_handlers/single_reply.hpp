606a95d009d13e042651824aba0ed046d21db44e
Petr Reznikov <prez@yandex-team.ru>
2016-04-26 16:17:37 +0300

    Splitted request_handler and reply_collector, preparing for chunked reply

--- app/request_handlers/single_reply.hpp
+++ app/request_handlers/single_reply.hpp
@@ -3,78 +3,42 @@
 
 #include <string>
 
-#include <http/server/reply.hpp>
 #include <http/server/request.hpp>
-
 #include <model/data/mailbox.h>
 
 namespace http {
 namespace server {
 
 /// The common handler for all incoming requests.
-template<typename Serializer>
+template<typename OnMessageFactory>
 class request_handler {
 public:
     request_handler(const request_handler&) = delete;
     request_handler& operator=(const request_handler&) = delete;
 
     /// Construct with a directory containing files to be served.
-    request_handler(Serializer s);
+    request_handler(OnMessageFactory f);
     request_handler(request_handler&& other);
 
     /// Handle a request and produce a reply.
     /// OnReply must be function object of type "void(reply)".
     template<typename OnReply>
-    void handle_request(const request& req, OnReply handler);
+    void handle_request(const request& req, OnReply&& handler);
 
 private:
     /// The directory containing the files to be served.
     using Mailbox = model::Mailbox<model::DummyImpl>;
     Mailbox mailbox;
-    Serializer serializer;
-
-    template<typename OnReply>
-    struct reply_collector {
-        OnReply handler;
-        Serializer serializer;
-        model::Messages messages;
-
-        reply_collector(OnReply h, Serializer s)
-            : handler(std::move(h)), serializer(std::move(s)) {}
-
-        template<typename Continuation>
-        void operator()(
-                boost::system::error_code e,
-                boost::optional<model::Message> m,
-                Continuation& cont) {
-            if (e) {
-                handler(reply::stock_reply(reply::internal_server_error));
-            } else if (!m) {
-                reply rep;
-                rep.content = serializer(std::move(messages));
-                fill_ok_reply(rep);
-                handler(rep);
-            } else {
-                messages.push_back(std::move(*m));
-                cont();
-            }
-        }
-    };
-
-    template<typename OnReply>
-    reply_collector<OnReply> make_reply_collector(OnReply&& h, Serializer s) {
-        return reply_collector<OnReply>( std::forward<OnReply>(h),std::move(s));
-    }
+    OnMessageFactory f;
 
     /// Perform URL-decoding on a string. Returns false if the encoding was
     /// invalid.
     static bool url_decode(const std::string& in, std::string& out);
-    static void fill_ok_reply(reply& rep);
 };
 
-template <typename S>
-request_handler<S> make_request_handler(S serializer) {
-    return request_handler<S>(std::move(serializer));
+template <typename OnMF>
+request_handler<OnMF> make_request_handler(OnMF&& on_message_factory) {
+    return request_handler<OnMF>(std::forward<OnMF>(on_message_factory));
 }
 
 } // namespace server
--- app/yreflection_single_reply_main.cpp
+++ app/yreflection_single_reply_main.cpp
@@ -1,13 +1,14 @@
 #include "templated_main.hpp"
 
+#include <app/request_handlers/single_reply.hpp>
+#include <app/request_handlers/reply_collector.hpp>
+
 #include <model/reflection/message.h>
 
 int main(int argc, char* argv[]) {
-    return templated_main(
-            argc,
-            argv,
-            make_request_handler([](const model::Messages& m) {
-                        return serialize(m).str();
-                    })
-    );
+    auto on_message_factory = make_reply_collector_factory([](const model::Messages& m) {
+        return serialize(m).str();
+    });
+    auto rh = make_request_handler( std::move(on_message_factory) );
+    return templated_main( argc, argv, std::move(rh) );
 }
