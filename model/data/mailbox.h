1676009de7c0635b2a1ea88e6081be87bbc91995
Sergei L. Khandrikov <thed636@yandex-team.ru>
2016-05-02 12:25:13 +0300

    Add Mailbox implementation with random messages
    Change-Id: I426bd3bd40edcc2b58ed4e5c728074f5c0949bbb

--- model/data/mailbox.h
+++ model/data/mailbox.h
@@ -22,13 +22,10 @@ class Mailbox {
 public:
     Mailbox(Impl impl) : impl(std::move(impl)) {}
 
-    template <typename Handler>
+    template <typename Handler, typename Continuation>
     struct OnMessage {
-        using Continuation = typename Impl::Continuation;
         Handler h;
         Continuation c;
-
-        OnMessage(Handler h) : h(std::move(h)), c() {}
 
         void operator() (error_code e, optional<Message> m = optional<Message>()) {
             h(e, std::move(m), std::move(*this));
@@ -41,12 +38,22 @@ public:
         }
     };
 
+    template <typename Handler>
+    struct OnMessageFactory {
+        Handler h;
+
+        template <typename Continuation>
+        OnMessage<Handler, Continuation> operator() (Continuation c) {
+            return OnMessage<Handler, Continuation>{std::move(h), std::move(c)};
+        }
+    };
+
     /**
      * This method is modeling heaviest query for all messages in mailbox
      */
     template <typename Handler>
     void getMessages(Handler h) const {
-        impl.getMessages(OnMessage<Handler>{std::move(h)});
+        impl.getMessages(OnMessageFactory<Handler>{std::move(h)});
     }
 
     /**
@@ -54,7 +61,7 @@ public:
      */
     template <typename Handler>
     void getMessages(const Message::Id& id, Handler h) const {
-        impl.getMessages(id, OnMessage<Handler>{std::move(h)});
+        impl.getMessages(id, OnMessageFactory<Handler>{std::move(h)});
     }
 
     /**
@@ -62,54 +69,99 @@ public:
      */
     template <typename Handler>
     void getMessages(const Recipient& r, Handler h) const {
-        impl.getMessages(r, OnMessage<Handler>{std::move(h)});
+        impl.getMessages(r, OnMessageFactory<Handler>{std::move(h)});
     }
 };
+
+
+inline std::string genRandomBody(const std::size_t len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    std::string retval(len, 0);
+    for (std::size_t i = 0; i < len; ++i) {
+        retval[i] = alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    return retval;
+}
+
+inline std::size_t getRandom(std::size_t min, std::size_t max) {
+    return min + (rand() % (std::size_t)(max - min + 1));
+}
+
+inline std::string genRandomBody() {
+    const std::size_t minLen = 1000;
+    const std::size_t maxLen = 19000;
+    return genRandomBody(getRandom(minLen, maxLen));
+}
 
 /**
  * Returns two default messages on any request
  */
 class DummyImpl {
 public:
-	template <typename OnMessage>
-	void getMessages(OnMessage h) const {
-	    h();
+	template <typename OnMessageFactory>
+	void getMessages(OnMessageFactory f) const {
+	    f(MultiplyRequest{getRandom(100, 1000)})();
 	}
 
-	template <typename OnMessage>
-	void getMessages(const Message::Id& /*id*/, OnMessage h) const {
-		getMessages(std::move(h));
+	template <typename OnMessageFactory>
+	void getMessages(const Message::Id& /*id*/, OnMessageFactory f) const {
+		f(SingleMessageRequest())();
 	}
 
-	template <typename OnMessage>
-	void getMessages(const Recipient& /*r*/, OnMessage h) const {
-		getMessages(std::move(h));
+	template <typename OnMessageFactory>
+	void getMessages(const Recipient& /*r*/, OnMessageFactory f) const {
+		f(MultiplyRequest{getRandom(1, 10)})();
 	}
 
-	struct Request : boost::asio::coroutine {
-	    Request() : boost::asio::coroutine() {}
+    struct SingleMessageRequest : boost::asio::coroutine {
+
+        template<typename Handler>
+        void operator()(Handler&& h) {
+            optional<Message> m = Message{
+                Message::Id{"42-100500"},
+                Message::Subject{"I love you Ozzy!"},
+                Message::Recipients{
+                    Recipient{Recipient::Type::from, Email{"Vasya Pupkin", "vasya@yandex.ru"}},
+                    Recipient{Recipient::Type::to, Email{"Ozzy Osbourne", "ozzy@gmail.com"}},
+                },
+                Message::Body{genRandomBody()}
+            };
+
+            reenter(*this) {
+                yield h(m);
+                yield h(optional<Message>());
+            }
+        }
+    };
+
+	struct MultiplyRequest : boost::asio::coroutine {
+        std::size_t count;
+
+        MultiplyRequest(std::size_t count) : count(count) {}
 
 	    template<typename Handler>
 	    void operator()(Handler&& h) {
-	        static optional<Message> m = Message{
-	            Message::Id{"42-100500"},
-	            Message::Subject{"I love you Ozzy!"},
-	            Message::Recipients{
-	                Recipient{Recipient::Type::from, Email{"Vasya Pupkin", "vasya@yandex.ru"}},
-	                Recipient{Recipient::Type::to, Email{"Ozzy Osbourne", "ozzy@gmail.com"}},
-	            },
-	            Message::Body{"You are the best, and the Black Sabbath is the best Sabbath in the world!"}
-	        };
 
 	        reenter(*this) {
-                yield h(m);
-                yield h(m);
+                for(;count;--count) {
+                    yield h( Message{
+                                Message::Id{"42-100500"},
+                                Message::Subject{"I love you Ozzy!"},
+                                Message::Recipients{
+                                    Recipient{Recipient::Type::from, Email{"Vasya Pupkin", "vasya@yandex.ru"}},
+                                    Recipient{Recipient::Type::to, Email{"Ozzy Osbourne", "ozzy@gmail.com"}},
+                                },
+                                Message::Body{genRandomBody()}
+                            });
+                };
                 yield h(optional<Message>());
 	        }
 	    }
 	};
-
-	using Continuation = Request;
 };
 
 
